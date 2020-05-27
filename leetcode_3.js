165cfec5c4f165be5ff63ea65736ff341272c39e
Asiah Bennett Dev <asiahbennettdev@gmail.com>
2020-05-27 14:02:31 -0400

    Update leetcode_3.js
    reverse integer

--- leetcode_3.js
+++ leetcode_3.js
@@ -4,7 +4,6 @@ Note:
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
-
 PREP
 Parameters: 32 bit signed integer
 Return: reverse digits of integer
